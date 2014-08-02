9730044d4ab23b3d04ef1abbdccd7972e3920cf5
Stephen Quenzer <swquenzer@gmail.com>
2014-08-02 08:42:57 -0400

    Finish sidebar functionality

--- js/main.js
+++ js/main.js
@@ -1,33 +1,54 @@
 //main.js
 $(document).ready(function() {
-	var canSlideOut = true;
-	var canSlideIn = false;
-	$('.left_sidebar').hover(function() {
-		//Mouseenter
-		if(canSlideOut) {
-			$(this).toggleClass('hover');
-			$('#main').toggleClass('shrink');
-			$('.left_sidebar h2').fadeIn("500");
-			var len = $('.left_sidebar section').length;
-			$('.left_sidebar section').each(function(i) {
-				$(this).delay(200*i+200).slideDown(200, function() {
-					if(i === len-1) {
-						canSlideIn = true;
+	var hover = false;
+	var isIn = true;
+	var isOut = false;
+
+	var slideOut = function() {
+		isIn = false;
+		$('.left_sidebar').toggleClass('hover', true);
+		$('#main').toggleClass('shrink', true);
+		$('.left_sidebar h2').fadeIn("200");
+		var len = $('.left_sidebar section').length;
+		$('.left_sidebar section').each(function(i) {
+			$(this).delay(200*i+300).slideDown(200, function() {
+				if(i === len-1) {
+					isOut = true;
+					//Is now fully out
+					if(!hover) {
+						slideIn();
 					}
-				});
+				}
 			});
+		});
+	}
+	var slideIn = function() {
+		isOut = false;
+		$('.left_sidebar h2').fadeOut(200, function() {
+			$('#main').toggleClass('shrink', false);
+			$('.left_sidebar').toggleClass('hover', false);
+			var count = 0;
+			$('.left_sidebar section').slideUp(300, function() {
+				//is now fully in
+				isIn = true;
+			});
+			isOut = false;
+		});
+	}
+
+	$('.left_sidebar').hover(function() {
+		if(isIn) {
+			hover = true;
+			slideOut();
 		}
 	}, function() {
-		//Mouseleave
-		if(canSlideIn) {
-			canSlideIn = false;
-			var that = $(this);
-			$('.left_sidebar h2').fadeOut(200, function() {
-				$('.left_sidebar section').slideToggle(200);
-				$('#main').toggleClass('shrink');
-				that.toggleClass('hover');
-				canSlideOut = true;
-			});
+		hover = false;
+		if(isOut) {
+			slideIn();
+			isOut = false;
 		}
 	});
+	window.thing = function() {
+		$('#content').append(document.properties);
+	}
 });
